0bc2fb75bfa2ff9c15cf43255a18d0e2b2f4d803
Michael Ladderbush <38595703+mikeladderbush@users.noreply.github.com>
2024-05-26 17:16:53 -0400

    Fixed uniqueness error

--- contacts/src/main/java/com/projectonecs320/Main.java
+++ contacts/src/main/java/com/projectonecs320/Main.java
@@ -29,38 +29,57 @@ public class Main {
 
         // Generate and add an appointment
         Appointment appointment = appointmentSupplier.get();
+        Appointment appointment2 = appointmentSupplier.get();
+        Appointment appointment3 = appointmentSupplier.get();
         appointmentService.addAppointment(appointment);
+        appointmentService.addAppointment(appointment2);
+        appointmentService.addAppointment(appointment3);
 
         // Generate and add a task
         Task task = taskSupplier.get();
+        Task task2 = taskSupplier.get();
+        Task task3 = taskSupplier.get();
+        Task task4 = taskSupplier.get();
+        Task task5 = taskSupplier.get();
+        Task task6 = taskSupplier.get();
         taskService.addTask(task);
+        taskService.addTask(task2);
+        taskService.addTask(task3);
+        taskService.addTask(task4);
+        taskService.addTask(task5);
+        taskService.addTask(task6);
 
         // Link task to appointment
         appointmentService.addTaskToAppointment(appointment.getId(), task.getId());
+        appointmentService.addTaskToAppointment(appointment.getId(), task2.getId());
+        appointmentService.addTaskToAppointment(appointment2.getId(), task3.getId());
+        appointmentService.addTaskToAppointment(appointment2.getId(), task4.getId());
+        appointmentService.addTaskToAppointment(appointment3.getId(), task5.getId());
+        appointmentService.addTaskToAppointment(appointment3.getId(), task6.getId());
+
 
         // Link appointment to contact
         contactService.addAppointmentToContact(contact.getId(), appointment.getId());
+        contactService.addAppointmentToContact(contact.getId(), appointment2.getId());
+        contactService.addAppointmentToContact(contact.getId(), appointment3.getId());
+
 
         // Retrieve all contacts, appointments, and tasks
-        System.out.println("Contacts:");
+        int contactNum = 1;
         for (Contact c : contactService.getAllContacts()) {
             System.out.println("Contact ID: " + c.getId());
+            contactNum++;
+            int appointmentNum = 1;
             for (Appointment a : appointmentService.getAllAppointments()) {
-                System.out.println("Contact's Appointment's IDs: " + a.getId());
+                System.out.println(c.getId() + "'s Appointment "+ appointmentNum + " " + a.getId());
+                appointmentNum++;
+                int taskNum = 1;
+                for (Task t : a.getTasks()) { // Iterate over tasks associated with the current appointment
+                    System.out.println(a.getId() + "'s Task " + taskNum + " " + t.getId());
+                    taskNum++;
+                }
             }
         }
-
-        System.out.println("\nAppointments:");
-        for (Appointment a : appointmentService.getAllAppointments()) {
-            System.out.println("Appointment ID: " + a.getId());
-            for (Task t : taskService.getAllTasks()) {
-                System.out.println("Appointment's Task's IDs: " + t.getId());
-            }
-        }
-
-        System.out.println("\nTasks:");
-        for (Task t : taskService.getAllTasks()) {
-            System.out.println("Task ID: " + t.getId());
-        }
+        
     }
 }
