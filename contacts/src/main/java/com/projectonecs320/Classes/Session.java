b627cd059ddb794cfa64f19ce59b5d1955fc34a6
Michael Ladderbush <38595703+mikeladderbush@users.noreply.github.com>
2024-06-05 21:37:39 -0400

    added sessions

--- contacts/src/main/java/com/projectonecs320/Classes/Session.java
+++ contacts/src/main/java/com/projectonecs320/Classes/Session.java
@@ -1,27 +1,47 @@
 package com.projectonecs320.Classes;
 
+import java.util.Date;
+
 /**
  * Represents a Training Session.
  */
 public class Session {
 
-    private String id;
+    protected String id;
+    protected String sessionType;
 
-    public static class SessionBuilder {
+    // Eventually set to a date/time that works best with database.
+    private String timeOfSession;
+    private Date dateOfSession;
+
+    protected Session(SessionBuilder<?> SessionBuilder) {
+        id = SessionBuilder.id;
+        sessionType = SessionBuilder.sessionType;
+        timeOfSession = SessionBuilder.timeOfSession;
+        dateOfSession = SessionBuilder.dateOfSession;
+    }
+
+    public static class SessionBuilder<T extends SessionBuilder<T>> {
 
         private String id;
+        private String sessionType;
+        private String timeOfSession;
+        private Date dateOfSession;
 
-        public SessionBuilder(String id) {
+        public SessionBuilder(String id, String sessionType, String timeOfSession, Date dateOfSession) {
             this.id = id;
+            this.sessionType = sessionType;
+            this.timeOfSession = timeOfSession;
+            this.dateOfSession = dateOfSession;
+        }
+
+        protected T self() {
+            return (T) this;
         }
 
         public Session buildSession() {
             return new Session(this);
         }
-    }
-
-    private Session(SessionBuilder SessionBuilder) {
-        id = SessionBuilder.id;
     }
 
     /**
@@ -41,4 +61,29 @@ public class Session {
     public void setId(String id) {
         this.id = id;
     }
+
+    public String getSessionType() {
+        return sessionType;
+    }
+
+    public void setSessionType(String sessionType) {
+        this.sessionType = sessionType;
+    }
+
+    public String getTimeOfSession() {
+        return timeOfSession;
+    }
+
+    public void setTimeOfSession(String timeOfSession) {
+        this.timeOfSession = timeOfSession;
+    }
+
+    public Date getDateOfSession() {
+        return dateOfSession;
+    }
+
+    public void setDateOfSession(Date dateOfSession) {
+        this.dateOfSession = dateOfSession;
+    }
+
 }
--- contacts/src/main/java/com/projectonecs320/Factories/SessionFactory.java
+++ contacts/src/main/java/com/projectonecs320/Factories/SessionFactory.java
@@ -1,6 +1,7 @@
 package com.projectonecs320.Factories;
 
 import java.util.function.Consumer;
+import java.util.Date;
 
 import com.projectonecs320.Classes.Session;
 import com.projectonecs320.Services.SessionService;
@@ -12,12 +13,18 @@ public class SessionFactory {
     // Method to create a Session with default values
     public static Session createSession() {
         String id = sessionService.generateId();
-        return new Session.SessionBuilder(id).buildSession();
+        String sessionType = "REGULAR CLASS";
+        String timeOfSession = "6:00PM";
+        Date dateOfSession = new Date();
+        return new Session.SessionBuilder<>(id, sessionType, timeOfSession, dateOfSession).buildSession();
     }
 
-    public static Session createSession(Consumer<Session.SessionBuilder> consumer) {
+    public static Session createSession(Consumer<Session.SessionBuilder<?>> consumer) {
         String id = sessionService.generateId();
-        Session.SessionBuilder builder = new Session.SessionBuilder(id);
+        String sessionType = "REGULAR CLASS";
+        String timeOfSession = "6:00PM";
+        Date dateOfSession = new Date();
+        Session.SessionBuilder<?> builder = new Session.SessionBuilder<>(id, sessionType, timeOfSession, dateOfSession);
         consumer.accept(builder);
         return builder.buildSession();
     }
